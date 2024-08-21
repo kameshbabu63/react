6207215b0e26a651c0ec6ed4963f5b70871fc386
kameshbabu63 <kameshbabu04032000@gmail.com>
2024-08-21 20:16:20 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Cards from './Cards/Card';
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+ <Cards />
+  </React.StrictMode>,
+)
